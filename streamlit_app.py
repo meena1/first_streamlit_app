c7c76031a9d00d400afd6a63b283278bc1fbf5d3
meena1 <meenakshimakharia@gmail.com>
2023-01-03 15:49:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ my_cur.execute("use warehouse pc_rivery_wh")
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fectchone()
 streamlit.text("List:")
-streamlit.dataframe(my_data_rows)
+streamlit.text(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add- ?')
 streamlit.write('The user entered ', fruit_choice)
